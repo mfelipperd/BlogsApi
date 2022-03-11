fd1fd35e8b6fc66a9651830bf3b42758ff3010b9
mfelipperd <felipperabelodurans@gmail.com>
2022-03-11 19:43:55 -0300

    cria validations com joi

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,5 +1,5 @@
 const Joi = require('@hapi/joi');
-const { User } = require('../models');
+const { user } = require('../models');
 
 const LENGTH_NAME = new Error();
 LENGTH_NAME.message = '"displayName" length must be at least 8 characters long';
@@ -10,15 +10,15 @@ EMAIL_INVALID.message = '"email" must be a valid email';
 EMAIL_INVALID.code = 400;
 
 const EMAIL_IS_REQUIRED = new Error();
-EMAIL_IS_REQUIRED.message = 'email is required';
-EMAIL_INVALID.code = 400;
+EMAIL_IS_REQUIRED.message = '"email" is required';
+EMAIL_IS_REQUIRED.code = 400;
 
 const PASSWORD_LENGTH = new Error();
-PASSWORD_LENGTH.message = 'password length must be 6 characters long';
+PASSWORD_LENGTH.message = '"password" length must be 6 characters long';
 PASSWORD_LENGTH.code = 400;
 
 const PASSWORD_IS_REQUIRED = new Error();
-PASSWORD_IS_REQUIRED.message = 'password is required';
+PASSWORD_IS_REQUIRED.message = '"password" is required';
 PASSWORD_IS_REQUIRED.code = 400;
 
 const ALREADY_EXISTS = new Error();
@@ -39,7 +39,8 @@ exports.validateEmail = async (email) => {
         emailIsRequired: Joi.required().error(EMAIL_IS_REQUIRED),
     });
     await schema.validateAsync({ emailValid: email, emailIsRequired: email });
-        if (await User.findOne({ where: { email } })) {
+        const emailExists = await user.findOne({ where: { email } });
+        if (emailExists) {
         throw ALREADY_EXISTS;
     } 
 };                  
