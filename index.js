6d26684d68dcfcf7955e70380dcd0b4c187ff9b2
mfelipperd <felipperabelodurans@gmail.com>
2022-03-14 19:06:16 -0300

    cria interçao do middle de verficacao do token

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userController = require('./controller/userController');
 const loginController = require('./controller/loginController');
+const { validateToken } = require('./schemas/tokenSchema');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/user', userController.createUser);
+app.get('/user', validateToken, userController.userList);
 app.post('/login', loginController.loginUser);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
