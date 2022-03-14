14b94db3680a7865f7c93f5aceb042215ab8e37d
mfelipperd <felipperabelodurans@gmail.com>
2022-03-14 15:17:17 -0300

    cria validacoes de login

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -25,6 +25,18 @@ const ALREADY_EXISTS = new Error();
 ALREADY_EXISTS.message = 'User already registered';
 ALREADY_EXISTS.code = 409;
 
+const EMAIL_LENGTH = new Error();
+EMAIL_LENGTH.message = '"email" is not allowed to be empty';
+EMAIL_LENGTH.code = 400;
+
+const PASSWORD_NOT_ALLOWED = new Error();
+PASSWORD_NOT_ALLOWED.message = '"password" is not allowed to be empty';
+PASSWORD_NOT_ALLOWED.code = 400;
+
+const INVALID_FIELDS = new Error();
+INVALID_FIELDS.message = 'Invalid fields';
+INVALID_FIELDS.code = 400;
+
 exports.validateDisplayName = async (displayName) => {
     const schema = Joi.object({
         lengthName: Joi.string().min(8).error(LENGTH_NAME),
@@ -43,7 +55,30 @@ exports.validateEmail = async (email) => {
         if (emailExists) {
         throw ALREADY_EXISTS;
     } 
-};                  
+};        
+
+exports.loginValidate = async (email) => {
+    const schema = Joi.object({
+        emailValid: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).error(EMAIL_LENGTH),
+        emailIsRequired: Joi.required().error(EMAIL_IS_REQUIRED),
+       // emailLength: Joi.min(3).error(EMAIL_LENGTH),
+    });
+    await schema
+    .validateAsync({ emailValid: email, emailIsRequired: email /* emailLength: email */ });
+        const emailExists = await user.findOne({ where: { email } });
+        if (!emailExists) {
+        throw INVALID_FIELDS;
+    } 
+};
+
+exports.loginPassword = async (password) => {
+    const schema = Joi.object({
+        passwordLenght: Joi.string().length(6).error(PASSWORD_NOT_ALLOWED),
+        passwordIsRequired: Joi.required().error(PASSWORD_IS_REQUIRED),
+    });
+    await schema.validateAsync({ passwordLenght: password, passwordIsRequired: password });
+};
 
 exports.validatePassword = async (password) => {
     const schema = Joi.object({
