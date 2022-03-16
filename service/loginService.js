af0c80d6773e39ac78e9e289069bba571ebfa41e
mfelipperd <felipperabelodurans@gmail.com>
2022-03-16 12:09:00 -0300

    altera login

--- service/loginService.js
+++ service/loginService.js
@@ -4,8 +4,6 @@ const loginUser = async (loginData) => {
     const { email, password } = loginData;
     const validPassword = await loginPassword(password);
     const validEmail = await loginValidate(email);
-    console.log(email, password);
-
     if (validEmail) return validEmail;
     if (validPassword) return validPassword;
     return { email, password };
