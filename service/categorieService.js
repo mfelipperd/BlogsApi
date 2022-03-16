5d0302d3f548d44467f3dc3d53dc3bb464ca9722
mfelipperd <felipperabelodurans@gmail.com>
2022-03-15 23:18:02 -0300

    cria service

---
+++ service/categorieService.js
@@ -0,0 +1,13 @@
+/* const { categorie } = require('../models');
+
+const newCategorie = async (name) => {
+    const categorieCreated = await categorie.create(name);
+    console.log(name);
+    // const newCategorieName = await categorie.findOne({ where: { name } });
+    return categorieCreated;
+};
+
+module.exports = {
+newCategorie,
+};
+*/
