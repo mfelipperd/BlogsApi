0994342b2d5144d0ccc36cb53d26c244f3390f32
mfelipperd <felipperabelodurans@gmail.com>
2022-03-16 14:23:59 -0300

    adciona includes e agradecimentos ao vitor diorio + link do pull request

--- service/blogPostService.js
+++ service/blogPostService.js
@@ -1,4 +1,4 @@
-const { blogPost } = require('../models');
+const { blogPost, user, categorie } = require('../models');
 
 const blogPostCreate = async (data, id) => {
     const { title, categoryIds, content } = data;
@@ -14,6 +14,20 @@ const blogPostCreate = async (data, id) => {
     return createNewPost;
 };
 
+const getAllPosts = async () => {
+const posts = await blogPost.findAll({
+    attributes: { exclude: ['userId'] },
+    include: [
+        { model: user, as: 'user', attributes: { exclude: ['password'] } },
+        { model: categorie, as: 'categories', through: [] }, // li o pull request do Vitor diorio para entender isso 
+    ], // pull do vitor https://github.com/tryber/sd-015-a-project-blogs-api/pull/104/files
+    
+});
+console.log(posts);
+return posts;
+};
+
 module.exports = {
     blogPostCreate,
+    getAllPosts,
 };
