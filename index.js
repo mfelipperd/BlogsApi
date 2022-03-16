d88ade1f71df5594aa87ae73dfadb9b90a527a9a
mfelipperd <felipperabelodurans@gmail.com>
2022-03-15 23:17:02 -0300

    cira rota do requisito 5

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userController = require('./controller/userController');
 const loginController = require('./controller/loginController');
+const categorieController = require('./controller/categorieController');
 const { validateToken } = require('./schemas/tokenSchema');
 const { userExists } = require('./schemas/userSchema');
+const { validateCategorieName } = require('./schemas/categorieSchema');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/user', userController.createUser);
 app.get('/user', validateToken, userController.userList);
-app.get('/user/:id', userExists, validateToken, userController.getUserById);
+app.get('/user/:id', validateToken, userExists, userController.getUserById);
 app.post('/login', loginController.loginUser);
+app.post('/categories', validateCategorieName, validateToken, categorieController.createCategorie);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 // não remova esse endpoint, e para o avaliador funcionar
