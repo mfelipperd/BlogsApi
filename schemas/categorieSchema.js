ab80aecb02d3f3b21f0da3b4759f150e347f2dba
mfelipperd <felipperabelodurans@gmail.com>
2022-03-16 12:06:50 -0300

    altera schmea de categorie

--- schemas/categorieSchema.js
+++ schemas/categorieSchema.js
@@ -2,9 +2,7 @@ const NAME_IS_REQUIRED = '"name" is required';
 
 exports.validateCategorieName = (req, res, next) => {
     const { name } = req.body;
-    console.log(name);
     if (!name || name.length === 0) {
         return res.status(400).json({ message: NAME_IS_REQUIRED });
     }
     next();
-};
